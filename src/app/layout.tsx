c50291e98ac7936d55f4dafc992de9f570208129
steven.downing@hotmail.com <steven.downing@hotmail.com>
2025-11-18 14:42:32 -0600

    update: Update favicon project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = {
   description: 'Crea y publica anuncios de bienes raíces desde tu celular en segundos usando inteligencia artificial',
   icons: {
     icon: [
-      { url: '/favicon.ico' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
     ],
